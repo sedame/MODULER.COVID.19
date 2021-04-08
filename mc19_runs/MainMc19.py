8fc1b58ca9de6555aa12221832405fb6b3a3cddb
sedame <wingsmans@htmail.com>
2021-04-08 22:38:51 +0100

    data aquisition module

---
+++ mc19_runs/MainMc19.py
@@ -0,0 +1,12 @@
+import subprocess
+#import DataInterface
+#import DataAcquisition
+#import DataEvoluAnalyzer
+#import DataLoder
+#import DataExplorer
+#subprocess.call("python","Modules_myscript.py")
+
+
+
+
+#DataInterface.case()
